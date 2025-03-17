386e6569bf2825ec7a437b38bce47f3c6f6ad13c
HutGib3941 <3941@trinity.croydon.sch.uk>
2025-03-17 22:25:52 +0000

    Add initial canvas clear webgl-code.js

--- webgl-code.js
+++ webgl-code.js
@@ -1,1 +1,10 @@
-//javascript
+main();
+
+function main() {
+  const canvas = document.getElementById("webgl-canvas");
+
+  const gl = canvas.getContext("webgl");
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0); //set clear background colour to opaque black
+  gl.clear(gl.COLOR_BUFFER_BIT); // Clear the color buffer with specified clear color
+}
